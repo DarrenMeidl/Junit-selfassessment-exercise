26f4527f9c61e8af197351161ac1bf3d3df65bc0
DarrenMeidl <dmeidl15@gmail.com>
2023-10-06 11:26:07 +0100

    Created 7 tests that all pass

--- src/FizzbuzzTest.java
+++ src/FizzbuzzTest.java
@@ -4,10 +4,83 @@ import static org.junit.jupiter.api.Assertions.*;
 class FizzbuzzTest {
     @Test
     void numEqualsFive() {
+        System.out.println("TEST 1: Num = 5");
         int num = 5;
         String expected = "buzz";
         Fizzbuzz f = new Fizzbuzz();
         String actual = f.fizzBuzz(num);
         assertEquals(expected, actual);
+        System.out.println("EXPECTED: "+expected+" ACTUAL: " + actual);
+        System.out.println("");
+    }
+    @Test
+    void numEqualsThree() {
+        System.out.println("TEST 2: Num = 3");
+        int num = 3;
+        String expected = "fizz";
+        Fizzbuzz f = new Fizzbuzz();
+        String actual = f.fizzBuzz(num);
+        assertEquals(expected, actual);
+        System.out.println("EXPECTED: "+expected+" ACTUAL: " + actual);
+        System.out.println("");
+    }
+    @Test
+    void numEqualsFifteen() {
+        System.out.println("TEST 3: Num = 15");
+        int num = 15;
+        String expected = "fizzbuzz";
+        Fizzbuzz f = new Fizzbuzz();
+        String actual = f.fizzBuzz(num);
+        assertEquals(expected, actual);
+        System.out.println("EXPECTED: "+expected+" ACTUAL: " + actual);
+        System.out.println("");
+    }
+
+    @Test
+    void numEqualsMinusThirty() {
+        System.out.println("TEST 4: Num = -30");
+        int num = -30;
+        String expected = "fizzbuzz";
+        Fizzbuzz f = new Fizzbuzz();
+        String actual = f.fizzBuzz(num);
+        assertEquals(expected, actual);
+        System.out.println("EXPECTED: "+expected+" ACTUAL: " + actual);
+        System.out.println("");
+    }
+
+    @Test
+    void numEqualsZero() {
+        System.out.println("TEST 5: Num = 0");
+        int num = 0;
+        String expected = "fizzbuzz";
+        Fizzbuzz f = new Fizzbuzz();
+        String actual = f.fizzBuzz(num);
+        assertEquals(expected, actual);
+        System.out.println("EXPECTED: "+expected+" ACTUAL: " + actual);
+        System.out.println("");
+    }
+
+    @Test
+    void numEqualsFour() {
+        System.out.println("TEST 6: Num = 4");
+        int num = 4;
+        String expected = "4";
+        Fizzbuzz f = new Fizzbuzz();
+        String actual = f.fizzBuzz(num);
+        assertEquals(expected, actual);
+        System.out.println("EXPECTED: "+expected+" ACTUAL: " + actual);
+        System.out.println("");
+    }
+
+    @Test
+    void numEqualsMinusSeven() {
+        System.out.println("TEST 7: Num = -7");
+        int num = -7;
+        String expected = "-7";
+        Fizzbuzz f = new Fizzbuzz();
+        String actual = f.fizzBuzz(num);
+        assertEquals(expected, actual);
+        System.out.println("EXPECTED: "+expected+" ACTUAL: " + actual);
+        System.out.println("");
     }
 }
