381251d8159b3b50e8568a4287d4aafd9a430491
DarrenMeidl <dmeidl15@gmail.com>
2023-10-06 11:15:06 +0100

    Fixed fizzBuzz(), completed 1 successful test

--- src/Fizzbuzz.java
+++ src/Fizzbuzz.java
@@ -3,20 +3,23 @@ public class Fizzbuzz {
     /*Random rand = new Random();
     public int num = rand.nextInt();*/
     public String fizzBuzz(int num){
-        if (num/3 != 0 && num/5 != 0){
-            return Integer.toString(num);
-        }
-        else if (num/3 == 0 && num/5 != 0){
-            String answer = "fizz";
+        String answer = "ERROR";
+        if (num%3 != 0 && num%5 != 0){
+            answer = Integer.toString(num);
             return answer;
         }
-        else if (num/3 != 0 && num/5 == 0){
-            String answer = "buzz";
+        else if (num%3 == 0 && num%5 != 0){
+            answer = "fizz";
             return answer;
         }
-        else if (num/3 == 0 && num/5 == 0){
-            String answer = "fizzbuzz";
+        else if (num%3 != 0 && num%5 == 0){
+            answer = "buzz";
             return answer;
         }
+        else if (num%3 == 0 && num%5 == 0){
+            answer = "fizzbuzz";
+            return answer;
+        }
+        return answer;
     }
 }
